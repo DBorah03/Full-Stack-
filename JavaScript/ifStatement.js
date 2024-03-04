601b2972b5252d472260244fd28fb3db4a602c1a
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-04 12:56:54 +0530

    js added

---
+++ JavaScript/ifStatement.js
@@ -0,0 +1,11 @@
+let light = "green";
+
+if (light == "red") {
+  console.log("Stop! The light is Red");
+}
+if (light == "orange") {
+  console.log("Slow down! The light is orange");
+}
+if (light == "green") {
+  console.log("Go! The light is green");
+}
