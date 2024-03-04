cf76733239d48c94254763627b86336fc83ca8b0
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-04 12:19:18 +0530

    js file added

---
+++ JavaScript/console.js
@@ -0,0 +1,10 @@
+console.log("Hi there! I am learning JavaScript");
+console.log("Now I am learing to use console.log property");
+
+let a = 3;
+let b = 6;
+
+sum = a + b;
+product = a * b;
+console.log("Sum is :", sum);
+console.log(`Product is ${product}`);
