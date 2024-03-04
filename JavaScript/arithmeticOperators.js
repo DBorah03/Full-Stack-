59fa02f5e77e2a352cfc7be2ce97bea48cbb1c7f
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-04 12:33:12 +0530

    js added

---
+++ JavaScript/arithmeticOperators.js
@@ -0,0 +1,10 @@
+let a = 9;
+let b = 3;
+console.log("The first number is 9 and the second number is 3.");
+
+console.log(`Addition of the numbers: ${a + b}`);
+console.log(`Substraction of the numbers: ${a - b}`);
+console.log(`Multiplication of the numbers: ${a * b}`);
+console.log(`Division of the numbers: ${a / b}`);
+console.log(`Modulo of the numbers: ${a % b}`);
+console.log(`Exponentiation of the numbers: ${a ** b}`);
