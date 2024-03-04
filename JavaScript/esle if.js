eba0f57fcd8001886ad2edf400b086a9ad466f5a
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-04 14:45:28 +0530

    else if added

---
+++ JavaScript/esle if.js
@@ -0,0 +1,11 @@
+let size = "l";
+
+if (size === "s") {
+  console.log("Price of popcorn is Rs. 50");
+} else if (size === "m") {
+  console.log("Price of popcorn is Rs. 100");
+} else if (size === "l") {
+  console.log("Price of popcorn is Rs. 210");
+} else {
+  console.log("Price of popcorn is Rs. 250");
+}
