82f3b94c7863cec6f719e6742744b8c02c0550fc
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-06 00:13:45 +0530

    file added

---
+++ JavaScript/Assignments/Q5.js
@@ -0,0 +1,17 @@
+let a = 3;
+let b = 21;
+let c = 33;
+
+if (a > b) {
+  if (a > c) {
+    console.log("a is the largest");
+  } else {
+    console.log("c is the largest");
+  }
+} else {
+  if (b > c) {
+    console.log("b is the largest");
+  } else {
+    console.log("c is the largest");
+  }
+}
