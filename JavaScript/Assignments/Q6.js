9c0dad1a844ba4c1aad3dacea0660dfe2d70521c
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-05 23:58:10 +0530

    file added

---
+++ JavaScript/Assignments/Q6.js
@@ -0,0 +1,8 @@
+let num1 = 3636;
+let num2 = 6366;
+
+if (num1 % 10 === num2 % 10) {
+  console.log(`${num1} and ${num2} have the same last digit `);
+} else {
+  console.log(`${num1} and ${num2} do not have the same last digit `);
+}
