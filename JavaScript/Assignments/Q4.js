95a4fe0fbc06b2ae2df0f6f9192405e58a613016
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-05 23:57:50 +0530

    file added

---
+++ JavaScript/Assignments/Q4.js
@@ -0,0 +1,7 @@
+let str = "alphabet";
+
+if ((str[0] === "A" || str[0] === "a") && str.length > 5) {
+  console.log("It is a golden string");
+} else {
+  console.log("It is not a golden string");
+}
