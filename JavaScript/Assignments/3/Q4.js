38c6b59fce659287cad4606b3d8e05c79b483bf1
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-08 11:47:45 +0530

    Q4 added

---
+++ JavaScript/Assignments/3/Q4.js
@@ -0,0 +1,8 @@
+let str = "Dhiraj Borah";
+let n = 7;
+
+if (str[n] === str[n].toLowerCase()) {
+  console.log("The character is in lower case");
+} else {
+  console.log("The character is in upper case");
+}
