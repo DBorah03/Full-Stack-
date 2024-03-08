7c37267ac176880987774d1035f0a329f30b2d54
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-08 12:11:50 +0530

    Q6 added

---
+++ JavaScript/Assignments/3/Q6.js
@@ -0,0 +1,8 @@
+let arr = ["Push", "Yourself", 1, 2, 3];
+let item = 3;
+
+if (arr.indexOf(item) != -1) {
+  console.log("Element exist in the array");
+} else {
+  console.log("Element doesn't exist in the array");
+}
