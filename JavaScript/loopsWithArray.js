406d6626ffe650b49e5f59a1118c265438323fe9
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-09 16:57:56 +0530

    file added

---
+++ JavaScript/loopsWithArray.js
@@ -0,0 +1,11 @@
+let character = [
+  "Thor",
+  "Iron Man",
+  "Captain America",
+  "Hulk",
+  "Hawkeye",
+  "Black Widow",
+];
+for (let i = 0; i < character.length; i++) {
+  console.log(i, character[i]);
+}
