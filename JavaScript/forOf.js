5a310a87a74a139d550a70a457c1b9105fe698dc
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-09 18:04:15 +0530

    for of added

--- JavaScript/forOf.js
+++ JavaScript/forOf.js
@@ -3,3 +3,11 @@ let fruits = ["mango", "appple", "litchi", "orange", "guava"];
 for (fruit of fruits) {
   console.log(fruit);
 }
+
+//another example on strings
+
+let str = "Dhiraj Borah";
+
+for (char of str) {
+  console.log(char);
+}
