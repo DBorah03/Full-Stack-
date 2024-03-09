fbb7d8a8f39c27577e4bff526e41971fb6b86a62
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-09 18:01:47 +0530

    new file added

---
+++ JavaScript/nestedForOf.js
@@ -0,0 +1,10 @@
+let heroes = [
+  ["Superman", "Batman", "Aquaman", "Flash"],
+  ["Thor", "Iron Man", "Captain America", "Hulk"],
+];
+
+for (list of heroes) {
+  for (division of list) {
+    console.log(division);
+  }
+}
