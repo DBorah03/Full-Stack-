5e0f91a78b0d03ee4db41671b6b2125712e3dd61
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-11 22:06:47 +0530

    file added

---
+++ JavaScript/nestedOjbects.js
@@ -0,0 +1,17 @@
+const classInfo = {
+  dhiraj: {
+    age: 25,
+    city: "ghy",
+  },
+  muluto: {
+    age: 24,
+    city: "dmp",
+  },
+  sobebar: {
+    age: 24,
+    city: "koj",
+  },
+};
+console.log(classInfo.dhiraj);
+console.log(classInfo.muluto);
+console.log(classInfo.sobebar);
