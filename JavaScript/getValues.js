561d49d28f4d13b3f3f690144ae86104c120b3b6
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-11 22:06:32 +0530

    file added

---
+++ JavaScript/getValues.js
@@ -0,0 +1,13 @@
+const student = {
+  name: "Dhiraj Borah",
+  age: 25,
+  sex: "Male",
+  occupation: "Software developer",
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.sex);
+console.log(student.occupation);
+
+console.log(student);
