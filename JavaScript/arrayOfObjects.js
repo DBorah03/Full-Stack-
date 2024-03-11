f2ac20330a142779b599a5db2205a2ae8c2f609c
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-11 22:06:19 +0530

    file added

---
+++ JavaScript/arrayOfObjects.js
@@ -0,0 +1,25 @@
+let classInfo = [
+  {
+    name: "Dhiraj",
+    age: 25,
+    sex: "male",
+  },
+  {
+    name: "Shruti",
+    age: 24,
+    sex: "female",
+  },
+  {
+    name: "David",
+    age: 27,
+    sex: "male",
+  },
+];
+
+console.log(classInfo);
+console.log(classInfo[0]);
+console.log(classInfo[1]);
+console.log(classInfo[2]);
+console.log(classInfo[0].name);
+console.log(classInfo[1].name);
+console.log(classInfo[2].name);
