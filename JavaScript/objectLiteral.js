fd250b3f52d3b5ac1af79ea8e586c21cde42f9b3
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-11 22:07:00 +0530

    file added

---
+++ JavaScript/objectLiteral.js
@@ -0,0 +1,13 @@
+const student = {
+  name: "Dhiraj Borah",
+  age: 25,
+  sex: "Male",
+  occupation: "Software developer",
+};
+
+const marks = {
+  eng: 98,
+  maths: 88,
+  science: 93,
+  total: [98, 88, 93],
+};
