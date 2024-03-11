14f421eca465c33ecf47bbb2daa1a429ca4e9bdb
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-11 22:07:18 +0530

    file added

---
+++ JavaScript/randomNumber.js
@@ -0,0 +1,15 @@
+//Random numbers between 1 to 100
+
+let random1 = Math.floor(Math.random() * 100) + 1;
+
+//Random numbers between 1 to 5
+
+let random2 = Math.floor(Math.random() * 5) + 1;
+
+//Random numbers between 21 to 25
+
+let random3 = Math.floor(Math.random() * 5) + 21;
+
+console.log(random1);
+console.log(random2);
+console.log(random3);
