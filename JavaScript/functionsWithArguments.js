bfa26c1b01e38d964d0aee6ca6d1dace968b90a4
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-12 22:25:51 +0530

    file added

---
+++ JavaScript/functionsWithArguments.js
@@ -0,0 +1,25 @@
+//function to disaplay name and age
+
+function info(name, age) {
+  console.log(`${name}'s age is ${age}`);
+}
+info("Dhiraj", 25);
+info("Sumit", 26);
+
+//function to print the average of three numbers
+
+function average(a, b, c) {
+  console.log((a + b + c) / 3);
+}
+average(3, 6, 12);
+average(66, 88, 90);
+
+//function that prints the multiplication table of a number
+
+function table(a) {
+  for (let i = a; i <= a * 10; i += a) {
+    console.log(i);
+  }
+}
+table(3);
+table(9);
