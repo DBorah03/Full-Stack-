9b0140d85501b333ae99428b64874619fad0ac78
Dhiraj Borah <dhirajborah12@gmail.com>
2024-03-12 22:25:37 +0530

    file added

---
+++ JavaScript/functions.js
@@ -0,0 +1,17 @@
+//function to print someting
+
+function printPoem() {
+  console.log("Jhonny Jhonny, yes papa.");
+  console.log("Eating sugar, no papa.");
+}
+printPoem();
+
+//function to role a dice and display the value of dice
+
+function dice() {
+  let num = Math.floor(Math.random() * 6) + 1;
+  console.log(num);
+}
+dice();
+
+dice();
