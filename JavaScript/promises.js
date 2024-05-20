139845fa68e50e934accece79f590e6fed7535a3
Dhiraj Borah <dhirajborah12@gmail.com>
2024-05-20 21:34:22 +0530

    file added

---
+++ JavaScript/promises.js
@@ -0,0 +1,10 @@
+function savetoDB(data) {
+  return new Promise((resolve, reject) => {
+    let intSpeed = Math.floor(Math.random() * 10) + 1;
+    if (intSpeed > 5) {
+      resolve("Success: Data was saved");
+    } else {
+      reject("Failure: Weak connection");
+    }
+  });
+}
