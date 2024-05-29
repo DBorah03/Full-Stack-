f1ffab3ecf35a1c4674dfe8115d2a5364c7011c4
Dhiraj Borah <dhirajborah12@gmail.com>
2024-05-29 12:25:07 +0530

    app.jsx modified

--- React/basic-react-app/src/App.jsx
+++ React/basic-react-app/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
+function Title() {
+  return <h1>I am Dhiraj Borah</h1>;
+}
+
 function App() {
-  return <button>Hello World!</button>;
+  return (
+    <div>
+      <button>Hello World!</button>
+      <Title />
+      <p>This is my story</p>
+    </div>
+  );
 }
 
 export default App;
