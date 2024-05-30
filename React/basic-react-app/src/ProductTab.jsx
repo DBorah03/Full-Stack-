77c269ef7f73c6a43197fefa70fe198a75053b3f
Dhiraj Borah <dhirajborah12@gmail.com>
2024-05-30 14:40:01 +0530

    Props learned

--- React/basic-react-app/src/ProductTab.jsx
+++ React/basic-react-app/src/ProductTab.jsx
@@ -1,9 +1,12 @@
 import Product from "./Product.jsx";
+
 function ProductTab() {
+  let opt1 = ["fast", "durable"];
+  let opt2 = { a: "sweet", b: "fresh" };
   return (
     <>
-      <Product title="Phone" price={42000} />
-      <Product title="Mango" price="30" />
+      <Product title="Phone" price={42000} feature={opt1} />
+      <Product title="Mango" price="30" feature={opt2.b} />
       <Product title="chicken" price={300} />
     </>
   );
