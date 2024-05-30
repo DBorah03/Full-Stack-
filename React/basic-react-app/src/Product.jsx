a9c38ff202fe7901059277d22a1afeda90b21c1e
Dhiraj Borah <dhirajborah12@gmail.com>
2024-05-30 15:28:25 +0530

    Dynamic component styling learned

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -1,8 +1,9 @@
 import "./Product.css";
 
 function Product({ title, price }) {
+  let styles = { backgroundColor: price > 3000 ? "yellow" : "" };
   return (
-    <div className="Product">
+    <div className="Product" style={styles}>
       <h2>{title}</h2>
       <h3>Price is: {price}</h3>
       {price > 3000 ? <p>21% discount</p> : ""}
