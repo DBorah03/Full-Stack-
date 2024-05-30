3187f5c3f8cc622b9e4f9ded2668461dbc9bd5eb
Dhiraj Borah <dhirajborah12@gmail.com>
2024-05-30 14:39:52 +0530

    Props learned

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -1,8 +1,9 @@
-function Product({ title, price }) {
+function Product({ title, price, feature }) {
   return (
     <div>
       <h2>{title}</h2>
       <h3>Price is: {price}</h3>
+      <h3>{feature}</h3>
     </div>
   );
 }
