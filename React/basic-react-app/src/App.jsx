7aee49a7a04ae11e451256b3ad7c899a16a47e58
Dhiraj Borah <dhirajborah12@gmail.com>
2024-05-30 14:39:41 +0530

    Props learned

--- React/basic-react-app/src/App.jsx
+++ React/basic-react-app/src/App.jsx
@@ -1,16 +1,9 @@
 import "./App.css";
-import Title from "./Title";
 import ProductTab from "./ProductTab.jsx";
 
 function App() {
-  let job = "software developer";
   return (
     <>
-      <button>Hello World!</button>
-      <Title />
-      <p>This is my story</p>
-      <p>I am a {job}</p>
-      <p>3*3 is {3 * 3}</p>
       <ProductTab />
     </>
   );
