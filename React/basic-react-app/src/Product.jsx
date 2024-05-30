a398fedb18ddb1ab728dcb6adc36bf70abbd7786
Dhiraj Borah <dhirajborah12@gmail.com>
2024-05-30 15:20:01 +0530

    Conditionals learned

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -1,11 +1,11 @@
 import "./Product.css";
 
-function Product({ title, price, feature }) {
+function Product({ title, price }) {
   return (
     <div className="Product">
       <h2>{title}</h2>
       <h3>Price is: {price}</h3>
-      <h3>{feature}</h3>
+      {price > 3000 ? <p>21% discount</p> : ""}
     </div>
   );
 }
