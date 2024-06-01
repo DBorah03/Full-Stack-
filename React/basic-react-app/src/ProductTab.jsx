2a1de7466c95938c6e2a381a8cb7ad6b023cec9d
Dhiraj Borah <dhirajborah12@gmail.com>
2024-06-01 22:03:00 +0530

    file updated

--- React/basic-react-app/src/ProductTab.jsx
+++ React/basic-react-app/src/ProductTab.jsx
@@ -1,13 +1,12 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
-  let opt1 = ["fast", "durable"];
-  let opt2 = { a: "sweet", b: "fresh" };
   return (
     <>
-      <Product title="Phone" price={42000} feature={opt1} />
-      <Product title="Mango" price="30" feature={opt2.b} />
-      <Product title="chicken" price={300} />
+      <Product title="Logitech MX Master" />
+      <Product title="Apple Pencil (2nd Gen)" />
+      <Product title="Zebronics Zeb-transformer" />
+      <Product title="Portronics Toad 23" />
     </>
   );
 }
