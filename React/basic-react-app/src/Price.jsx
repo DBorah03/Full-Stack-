71ad0a61049140bce4d1e3f431297c73b851d72e
Dhiraj Borah <dhirajborah12@gmail.com>
2024-06-02 00:37:27 +0530

    basic app completed

--- React/basic-react-app/src/Price.jsx
+++ React/basic-react-app/src/Price.jsx
@@ -1,9 +1,16 @@
 export default function Price({ oldPrice, newPrice }) {
+  let oldStyle = { textDecorationLine: "line-through" };
+  let newStyle = { fontWeight: "bold" };
+  let styles = {
+    backgroundColor: "#e0c367",
+    borderBottomLeftRadius: "15px",
+    borderBottomRightRadius: "15px",
+  };
   return (
-    <div>
-      <span>{oldPrice}</span>
+    <div style={styles}>
+      <span style={oldStyle}>{oldPrice}</span>
       &nbsp; &nbsp; &nbsp;
-      <span>{newPrice}</span>
+      <span style={newStyle}>{newPrice}</span>
     </div>
   );
 }
