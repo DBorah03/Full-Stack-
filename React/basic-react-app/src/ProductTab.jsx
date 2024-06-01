33a9b59c1895a4ed426165aba9f75ec83bd989a6
Dhiraj Borah <dhirajborah12@gmail.com>
2024-06-02 00:38:08 +0530

    basic app completed

--- React/basic-react-app/src/ProductTab.jsx
+++ React/basic-react-app/src/ProductTab.jsx
@@ -1,13 +1,17 @@
 import Product from "./Product";
 
 function ProductTab() {
+  let styles = {
+    display: "flex",
+    flexWrap: "wrap",
+  };
   return (
-    <>
+    <div style={styles}>
       <Product title="Logitech MX Master" idx={0} />
       <Product title="Apple Pencil (2nd Gen)" idx={1} />
       <Product title="Zebronics Zeb-transformer" idx={2} />
       <Product title="Portronics Toad 23" idx={3} />
-    </>
+    </div>
   );
 }
 
